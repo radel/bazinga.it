ad4ea8f3da5879d35da5d04e82022092054d7b8e
Marco Bonomo <marco.bonomo@gmail.com>
2023-02-28 13:29:41 +0100

    loading lazy

--- components/content/ProseImg.vue
+++ components/content/ProseImg.vue
@@ -1,6 +1,7 @@
 <template>
     <NuxtImg v-bind="$attrs"    
         fit="cover"
+        loading="lazy"
         class="w-full rounded shadow cursor-pointer"
         sizes="sm:100vw md:50vw lg:900px"
         :modifiers="{rotate: null}"
