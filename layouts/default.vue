8b7daab9978840d6314cd4d112251488430e7edf
Marco Bonomo <marco.bonomo@gmail.com>
2024-01-04 22:51:23 +0100

    no line

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,9 @@
 
     <div class="">
       <slot />
-      <DefaultSection>
         <div class="text-center mx-auto max-w-4xl py-8">
             <a href="/about">about</a> · <a href="https://www.instagram.com/marco__bnm">instagram</a> · <a href="https://www.threads.net/marco__bnm">threads</a> · <a href="https://www.twitter.com/radel">twitter</a>
         </div>
-      </DefaultSection>
     </div>
 
     
