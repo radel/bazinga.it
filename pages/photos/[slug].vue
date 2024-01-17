231c1352a72de405ec9a8cb713647220a689b556
Marco Bonomo <marco.bonomo@gmail.com>
2024-01-17 17:53:24 +0100

    html redirect

--- pages/photos/[slug].vue
+++ pages/photos/[slug].vue
@@ -1,7 +1,8 @@
-
+<template>
+        <meta http-equiv="refresh" :content="`0;url=/${slug}`">
+</template>
 <script setup>
 const { params } = useRoute()
 const { slug } = params
-await navigateTo('/photos')
 
 </script>
